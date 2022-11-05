9d751508b8a5c8003dd6f1532a6e98c4ddf6c3f3
Angela <balderasangela@gmail.com>
2022-11-04 23:44:52 -0400

    COMPLETED ASSIGNMENT

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let num3 = 14
+let num4 = 5
+let multiply = num1 * num2
+
+let random = Math.round(Math.random() * 10) + 1 
+
+let mod = num3 % num4
+
+let max = Math.max(2,14,17,3,20)
+
+
+
+
+
